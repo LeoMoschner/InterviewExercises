905048aa9627a7dd9344c0816831ef57ad25e9c6
LeoMoschner <l.espindolamoschner@gmail.com>
2023-01-03 14:49:12 -0300

    some new exercises

--- src/main/java/Lean/fizzBuzz.java
+++ src/main/java/Lean/fizzBuzz.java
@@ -1,4 +1,26 @@
 package Lean;
 
-public class fizzBuzz {
+public class FizzBuzz {
+    public static void main(String[] args) {
+        solution(100);
+    }
+
+    public static void solution(int range){
+        for (int i = 0; i < range; i++){
+            StringBuilder aux = new StringBuilder();
+            boolean aux1 = false;
+            aux.append(i + ": ");
+            if (i % 3 == 0){
+                aux.append("Fizz");
+                aux1 = true;
+            }
+            if (i % 5 == 0){
+                aux.append("Buzz");
+                aux1 = true;
+            }
+            if (aux1){
+                System.out.println(aux);
+            }
+        }
+    }
 }
