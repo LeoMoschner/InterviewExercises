6fbd32e3a6b1a712c891a0b70c9c2dece7cb6dcc
LeoMoschner <l.espindolamoschner@gmail.com>
2023-01-20 11:12:47 -0300

    practicanding

--- src/main/java/Lean/SortSelection.java
+++ src/main/java/Lean/SortSelection.java
@@ -1,21 +1,19 @@
 package Lean;
 
 import java.util.Arrays;
-
 public class SortSelection {
     public static void main(String[] args) {
         int [] test = {3, 4, 7, 1, 2};
         System.out.println(Arrays.toString(selectionSort(test)));
     }
-
     public static int[] selectionSort(int[] inputArray){
         int length = inputArray.length;
         for (int i = 0; i < length - 1; i++){
-            int minor = inputArray[i];
+            int minorValue = inputArray[i];
             int minorIndex = i;
             for (int j = i + 1; j < length;  j++){
-                if (inputArray[j] < minor){
-                    minor = inputArray[j];
+                if (inputArray[j] < minorValue){
+                    minorValue = inputArray[j];
                     minorIndex = j;
                 }
             }
